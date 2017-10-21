31c4abcedf190617a81986e36b6cd3e0f694c0d4
Boris Kaul <localvoid@gmail.com>
2017-10-21 08:51:41 +0700

    Index all ONE_OF(schemas...) in Bundle

--- packages/pck/src/bundle.ts
+++ packages/pck/src/bundle.ts
@@ -5,12 +5,19 @@ export class Bundle {
   readonly schemas: Schema[];
   readonly types: Set<Type>;
   readonly fixedSizeSchemas: Set<Schema>;
+  readonly oneOfSchemas: Map<Schema, number>;
   readonly index: Map<string, Schema>;
 
-  constructor(schemas: Schema[], types: Set<Type>, fixedSizeSchemas: Set<Schema>) {
+  constructor(
+    schemas: Schema[],
+    types: Set<Type>,
+    fixedSizeSchemas: Set<Schema>,
+    oneOfSchemas: Map<Schema, number>,
+  ) {
     this.schemas = schemas;
     this.types = types;
     this.fixedSizeSchemas = fixedSizeSchemas;
+    this.oneOfSchemas = oneOfSchemas;
     this.index = new Map<string, Schema>();
 
     for (const schema of schemas) {
@@ -24,21 +31,33 @@ export class Bundle {
   findSchemaByName(name: string): Schema | undefined {
     return this.index.get(name);
   }
+
+  isOneOfSchema(schema: Schema): number | undefined {
+    return this.oneOfSchemas.get(schema);
+  }
 }
 
 export function bundle(schemas: Schema[]): Bundle {
   const analyzeResult = analyzeSchemas(schemas);
-  return new Bundle(schemas, analyzeResult.types, analyzeResult.fixedSizeSchemas);
+  return new Bundle(
+    schemas,
+    analyzeResult.types,
+    analyzeResult.fixedSizeSchemas,
+    analyzeResult.oneOfSchemas,
+  );
 }
 
 interface AnalyzeResult {
-  types: Set<Type>;
-  fixedSizeSchemas: Set<Schema>;
+  readonly types: Set<Type>;
+  readonly fixedSizeSchemas: Set<Schema>;
+  readonly oneOfSchemas: Map<Schema, number>;
 }
 
 function analyzeSchemas(schemas: Schema[]): AnalyzeResult {
   const types = new Set<Type>();
   const fixedSizeSchemas = new Set<Schema>();
+  const oneOfSchemas = new Map<Schema, number>();
+  let oneOfIndex = 0;
 
   for (const schema of schemas) {
     if (!schema.hasDynamicSize()) {
@@ -47,11 +66,20 @@ function analyzeSchemas(schemas: Schema[]): AnalyzeResult {
 
     for (const field of schema.fields) {
       types.add(field.type);
+
+      if (field.type.isOneOf()) {
+        for (const oneOfType of field.type.props) {
+          if (oneOfType.isRef()) {
+            oneOfSchemas.set(oneOfType.props, oneOfIndex++);
+          }
+        }
+      }
     }
   }
 
   return {
     types,
     fixedSizeSchemas,
+    oneOfSchemas,
   };
 }
